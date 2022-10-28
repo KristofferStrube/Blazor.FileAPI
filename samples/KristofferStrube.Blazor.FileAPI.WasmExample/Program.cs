3531688b954054f40aafc584a1d4fcabdd9e8bdb
KristofferStrube <kristoffer.strube@gmail.com>
2022-10-28 13:24:50 +0200

    Updated sample to use URL as service.

--- samples/KristofferStrube.Blazor.FileAPI.WasmExample/Program.cs
+++ samples/KristofferStrube.Blazor.FileAPI.WasmExample/Program.cs
@@ -1,3 +1,4 @@
+using KristofferStrube.Blazor.FileAPI;
 using KristofferStrube.Blazor.FileAPI.WasmExample;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -8,5 +9,6 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<URL>();
 
 await builder.Build().RunAsync();
