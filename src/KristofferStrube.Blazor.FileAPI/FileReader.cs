d6a3c168690102287c441e267166a41ddce50321
KristofferStrube <kristoffer.strube@gmail.com>
2022-11-01 16:21:48 +0100

    Extended sample of FileReader

--- src/KristofferStrube.Blazor.FileAPI/FileReader.cs
+++ src/KristofferStrube.Blazor.FileAPI/FileReader.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.JSInterop;
+using System.Text.Json.Serialization;
 
 namespace KristofferStrube.Blazor.FileAPI;
 
@@ -27,7 +28,9 @@ public class FileReader : BaseJSWrapper
     {
         IJSObjectReference helper = await jSRuntime.GetHelperAsync();
         IJSObjectReference jSInstance = await helper.InvokeAsync<IJSObjectReference>("constructFileReader");
-        return new FileReader(jSRuntime, jSInstance);
+        var fileReader = new FileReader(jSRuntime, jSInstance);
+        await helper.InvokeVoidAsync("registerEventHandlers", fileReader, jSInstance);
+        return fileReader;
     }
 
     /// <summary>
@@ -35,7 +38,11 @@ public class FileReader : BaseJSWrapper
     /// </summary>
     /// <param name="jSRuntime">An <see cref="IJSRuntime"/> instance.</param>
     /// <param name="jSReference">A JS reference to an existing <see cref="FileReader"/>.</param>
-    internal FileReader(IJSRuntime jSRuntime, IJSObjectReference jSReference) : base(jSRuntime, jSReference) { }
+    internal FileReader(IJSRuntime jSRuntime, IJSObjectReference jSReference) : base(jSRuntime, jSReference) {
+        ObjRef = DotNetObjectReference.Create(this);
+    }
+
+    public DotNetObjectReference<FileReader> ObjRef { get; init; }
 
     public async Task ReadAsArrayBufferAsync(Blob blob)
     {
@@ -69,6 +76,102 @@ public class FileReader : BaseJSWrapper
     public async Task<ushort> GetReadyStateAsync()
     {
         IJSObjectReference helper = await helperTask.Value;
-        return await helper.InvokeAsync<ushort>("readyState");
+        return await helper.InvokeAsync<ushort>("getAttribute", JSReference, "readyState");
     }
+
+    /// <summary>
+    /// Checks whether the result is a either a string or a byte array.
+    /// </summary>
+    /// <returns>Either the type of <see langword="string"/> or type of <see cref="byte"/>[].</returns>
+    public async Task<Type?> GetResultTypeAsync()
+    {
+        IJSObjectReference helper = await helperTask.Value;
+        var isArrayBuffer = await helper.InvokeAsync<bool>("isArrayBuffer", JSReference);
+        return isArrayBuffer ? typeof(byte[]) : typeof(string);
+    }
+
+    public async Task<string?> GetResultAsStringAsync()
+    {
+        IJSObjectReference helper = await helperTask.Value;
+        return await helper.InvokeAsync<string>("getAttribute", JSReference, "result");
+    }
+
+    public async Task<byte[]?> GetResultAsByteArrayAsync()
+    {
+        IJSObjectReference helper = await helperTask.Value;
+        var jSResult = await helper.InvokeAsync<IJSObjectReference>("getAttribute", JSReference, "result");
+        return await helper.InvokeAsync<byte[]>("arrayBuffer", jSResult);
+    }
+
+    /// <summary>
+    /// Gets the error object reference which will be null if no error occured.
+    /// </summary>
+    /// <returns>A nullable IJSObjectReference because it was out of scope to wrap the Exception API.</returns>
+    public async Task<IJSObjectReference?> GetErrorAsync()
+    {
+        IJSObjectReference helper = await helperTask.Value;
+        return await helper.InvokeAsync<IJSObjectReference?>("getAttribute", JSReference, "error");
+    }
+
+    [JsonIgnore]
+    public Func<ProgressEvent, Task>? OnLoadStart { get; set; }
+
+    [JsonIgnore]
+    public Func<ProgressEvent, Task>? OnProgress { get; set; }
+
+    [JsonIgnore]
+    public Func<ProgressEvent, Task>? OnLoad { get; set; }
+
+    [JsonIgnore]
+    public Func<ProgressEvent, Task>? OnAbort { get; set; }
+
+    [JsonIgnore]
+    public Func<ProgressEvent, Task>? OnError { get; set; }
+
+    [JsonIgnore]
+    public Func<ProgressEvent, Task>? OnLoadEnd { get; set; }
+
+    [JSInvokable]
+    public async Task InvokeOnLoadStart(IJSObjectReference jsProgressEvent)
+    {
+        if (OnLoadStart is null) return;
+        await OnLoadStart.Invoke(new ProgressEvent(jSRuntime, jsProgressEvent));
+    }
+
+    [JSInvokable]
+    public async Task InvokeOnProgress(IJSObjectReference jsProgressEvent)
+    {
+        if (OnProgress is null) return;
+        await OnProgress.Invoke(new ProgressEvent(jSRuntime, jsProgressEvent));
+    }
+
+    [JSInvokable]
+    public async Task InvokeOnLoad(IJSObjectReference jsProgressEvent)
+    {
+        if (OnLoad is null) return;
+        await OnLoad.Invoke(new ProgressEvent(jSRuntime, jsProgressEvent));
+    }
+
+    [JSInvokable]
+    public async Task InvokeOnAbort(IJSObjectReference jsProgressEvent)
+    {
+        if (OnAbort is null) return;
+        await OnAbort.Invoke(new ProgressEvent(jSRuntime, jsProgressEvent));
+    }
+
+    [JSInvokable]
+    public async Task InvokeOnError(IJSObjectReference jsProgressEvent)
+    {
+        if (OnError is null) return;
+        await OnError.Invoke(new ProgressEvent(jSRuntime, jsProgressEvent));
+    }
+
+    [JSInvokable]
+    public async Task InvokeOnLoadEnd(IJSObjectReference jsProgressEvent)
+    {
+        if (OnLoadEnd is null) return;
+        await OnLoadEnd.Invoke(new ProgressEvent(jSRuntime, jsProgressEvent));
+    }
+
+
 }
--- src/KristofferStrube.Blazor.FileAPI/wwwroot/KristofferStrube.Blazor.FileAPI.js
+++ src/KristofferStrube.Blazor.FileAPI/wwwroot/KristofferStrube.Blazor.FileAPI.js
@@ -16,3 +16,16 @@ export function constructFile(blobParts, fileName, options) {
 export function constructFileReader() {
     return new FileReader();
 }
+
+export function registerEventHandlers(fileReader, jSInstance) {
+    jSInstance.addEventListener('loadstart', (e) => fileReader.objRef.invokeMethodAsync('InvokeOnLoadStart', DotNet.createJSObjectReference(e)));
+    jSInstance.addEventListener('progress', (e) => fileReader.objRef.invokeMethodAsync('InvokeOnProgress', DotNet.createJSObjectReference(e)));
+    jSInstance.addEventListener('load', (e) => fileReader.objRef.invokeMethodAsync('InvokeOnLoad', DotNet.createJSObjectReference(e)));
+    jSInstance.addEventListener('abort', (e) => fileReader.objRef.invokeMethodAsync('InvokeOnAbort', DotNet.createJSObjectReference(e)));
+    jSInstance.addEventListener('error', (e) => fileReader.objRef.invokeMethodAsync('InvokeOnError', DotNet.createJSObjectReference(e)));
+    jSInstance.addEventListener('loadend', (e) => fileReader.objRef.invokeMethodAsync('InvokeOnLoadEnd', DotNet.createJSObjectReference(e)));
+}
+
+export function isArrayBuffer(fileReader) {
+    return (fileReader.result instanceof ArrayBuffer)
+}
